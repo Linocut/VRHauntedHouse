00a6413b91b7d84f14821344b7dadbb04f471f9e
DRATHAH <xavier@petersonfoundation.com>
2023-10-12 21:25:39 -0400

    Jumpscare Code
    Finalized code for the jumpscare

--- LargeRoom/Assets/Scripts/Jumpscare.cs
+++ LargeRoom/Assets/Scripts/Jumpscare.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class Jumpscare : MonoBehaviour
 {
-    public float jitterTime = .1f;
+    [Header("Things to set")]
     public Animator jumpscareAnimator;
+    public AudioSource jumpscareAudio;
+    public float jitterTime = .1f;
     public float offset = 1f;
     public float moveSpeed = 20f;
     public float jitterAmount = 10;
-    public float ogFOV;
 
     Transform playerCam;
     private Transform lookPos;
@@ -19,6 +20,7 @@ public class Jumpscare : MonoBehaviour
     private Player player;
     private Vector3 ogPos;
     private Quaternion ogRot;
+    private Vector3 ogCamPos;
 
     // Start is called before the first frame update
     void Start()
@@ -30,37 +32,21 @@ public class Jumpscare : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (jumping && finishedLoop)
         {
-            StartCoroutine(JitterDelay(playerCam.eulerAngles.y));
+            StartCoroutine(JitterDelay(playerCam.position.y));
         }
         else if (!jumping)
         {
-            ogFOV = playerCam.transform.parent.eulerAngles.y;
+            ogCamPos = playerCam.transform.parent.transform.position;
         }
     }
 
     private IEnumerator JitterDelay(float currentRot)
     {
         finishedLoop = false;
-
-        float targetRot = playerCam.eulerAngles.y;
-        if (currentRot > ogFOV)
-        {
-            targetRot -= jitterAmount * 2;
-        }
-        else if (currentRot < ogFOV)
-        {
-            targetRot += jitterAmount * 2 ;
-        }
-        else if (currentRot == ogFOV)
-        {
-            targetRot -= jitterAmount;
-        }
-
-        playerCam.eulerAngles = new Vector3(playerCam.eulerAngles.x, targetRot);
-
+        float degree = Random.Range(-jitterAmount, jitterAmount); // Determines a random value between two values to jitter the camera
+        playerCam.position = new Vector3(ogCamPos.x, ogCamPos.y, ogCamPos.z + degree); // Moves the camera according to 'degree' value
 
         yield return new WaitForSeconds(jitterTime);
         finishedLoop = true;
@@ -73,14 +59,14 @@ public class Jumpscare : MonoBehaviour
             player = other.transform.root.GetComponent<Player>();
             player.canMove = false;
             // Determine an appropriate distance in front of the camera
-            Vector3 pos = lookPos.position + new Vector3(lookPos.forward.x, Mathf.Clamp(lookPos.forward.y, -.5f, -.25f), lookPos.forward.z).normalized * offset;
+            Vector3 pos = lookPos.position + new Vector3(lookPos.forward.x, -lookPos.position.y + transform.position.y, lookPos.forward.z).normalized * offset;
 
             // Get creature's original position and rotation
             ogPos = transform.position;
             ogRot = transform.rotation;
 
             // Moves creature in front of camera, but not instantly
-            transform.position = Vector3.Lerp(transform.position, pos, moveSpeed);
+            transform.position = pos;
             
             RunJumpscare();
         }
@@ -88,14 +74,18 @@ public class Jumpscare : MonoBehaviour
 
     void RunJumpscare()
     {
+        if (jumpscareAudio)
+        {
+            jumpscareAudio.Play();
+        }
         jumping = true;
-        transform.LookAt(lookPos);
+        transform.LookAt(lookPos.Find("Target"));
         jumpscareAnimator.SetTrigger("Jumpscare"); // Play creature's animation
     }
 
     public void ReturnToPosition()
     {
-        playerCam.eulerAngles = new Vector3(0, 0);
+        playerCam.transform.position = ogCamPos;
         transform.position = ogPos;
         transform.rotation = ogRot;
         player.canMove = true;
